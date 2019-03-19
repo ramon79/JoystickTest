34659075bd778236033467795cb98b49660e08ab
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-19 22:46:16 +0900

    done

---
+++ JoystickTest/Assets/Standard Assets/CrossPlatformInput/Scripts/BallController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BallController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float hInput = CrossPlatformInputManager.GetAxis("Horizontal") * speed;
+        float vInput = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+
+        rb.AddForce(hInput, 0, vInput);
+
+    }
+}
